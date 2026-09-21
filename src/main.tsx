a13fe477de5f46ee2f6dfaf093314a0c812f4fbe
Abhishek-thakur01 <ak19813528@gmail.com>
2026-09-21 16:15:25 +0530

    Update main to support /admin route for CMS

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Admin from "./Admin";
+
+const isAdmin = window.location.pathname.includes("/admin");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {isAdmin ? <Admin /> : <App />}
   </StrictMode>
 );
